f2a3b96681800c02e9208ff3120a434fa33ecf3c
Ashish Singh Panwar <ashish.panwar@intelligrape.com>
2014-09-27 15:06:27 +0530

    Initial codebase commit

---
+++ bundle/src/main/java/com/ig/intellimeet/HelloService.java
@@ -0,0 +1,13 @@
+package com.ig.intellimeet;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
